0522bc2c106038594c9b6864318780b4fc14d2c3
alina-t-872 <115736166+drunkenmedusa@users.noreply.github.com>
2022-10-19 12:43:14 +0300

    Updated structure for lab 2

---
+++ lab2/src/task4/Book.java
@@ -0,0 +1,18 @@
+package task4;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String toString() {
+        // TODO
+        return null;
+    }
+}
---
+++ lab2/src/task4/Main.java
@@ -0,0 +1,11 @@
+package task4;
+
+public class Main {
+    public static void main(String[] args) {
+        Book b1 = new Book("Morometii", "Marin Preda", 1955);
+        System.out.println(b1);
+
+        Book b2 = new Book("Levantul", "Mircea Cărtărescu", 1990);
+        System.out.println(b2);
+    }
+}
